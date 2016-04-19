6e72870e3bdab50d3bf5039dccabb496046416b6
yjose <yjosejose6@gmail.com>
2016-04-19 12:29:04 +0100

    Modification de front-end de site
    + page d'accueil
    + page encore de construction
    + suppression des fichiers inutiles

---
+++ resources/views/home/under_construction.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container center-block" style="margin-top: 200px;margin-bottom: 200px;">
+    <img src="{{asset('img/under_construction.png')}}" class=" center-block" alt="" width="350px" />
+    </div>
+@endsection
