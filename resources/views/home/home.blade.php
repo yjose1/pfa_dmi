05ccc14b36f2692fd083355845a3e3b250e26d73
yjose1 <yjosejose@gmail.com>
2016-04-19 12:56:24 +0100

    Revert "Revert "Modification de front-end de site""
    This reverts commit f2fdd3a99d7903384e30a46ee0ce0710f4492585.

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+@section('style')
+@endsection
+
+
+@section('content')
 @section('content')
     <!--=========== BEGIN SLIDER SECTION ================-->
 
@@ -7,10 +12,6 @@
         <div class="row">
             <div class="col-lg-12 col-md-12">
                 <div class="slider_area">
-
-
-
-
                     <!-- Start super slider -->
                     <div id="slides">
                         <ul class="slides-container">
@@ -66,17 +67,11 @@
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="newsfeed_area wow fadeInRight">
-                        <ul class="nav nav-tabs feed_tabs" id="myTab2">
-                            <li class="active"><a href="#news" data-toggle="tab">News</a></li>
-                            <li><a href="#notice" data-toggle="tab">Notice</a></li>
-                            <li><a href="#events" data-toggle="tab">Events</a></li>
+                        <ul class="nav nav-tabs feed_tabs nav-justified " id="myTab2">
+                            <li class="active"><a href="#news" data-toggle="tab">Actualités</a></li>
+                            <li><a href="#notice" data-toggle="tab">Notifications</a></li>
+                            <li><a href="#events" data-toggle="tab">Evenements</a></li>
                         </ul>
-
-
-
-
-
-
                         <!-- Tab panes -->
                         <div class="tab-content">
                             <!-- Start news tab content -->
@@ -91,7 +86,8 @@
                                             </div>
                                             <div class="media-body">
                                                 <a href="#">Dummy text of the printing and typesetting industry</a>
-                                                <span class="feed_date">27.02.15</span>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cumque cupiditate dolor eligendi harum laborum .....</p>
+                                                <span class="feed_date"> <i class="fa fa-clock-o"></i> 27.02.15</span>
                                             </div>
                                         </div>
                                     </li>
@@ -126,7 +122,6 @@
                             </div>
                             <!-- Start notice tab content -->
                             <div class="tab-pane fade " id="notice">
-                                <div class="single_notice_pane">
                                     <ul class="news_tab">
                                         <li>
                                             <div class="media">
@@ -168,48 +163,7 @@
                                             </div>
                                         </li>
                                     </ul>
-                                    <ul class="news_tab">
-                                        <li>
-                                            <div class="media">
-                                                <div class="media-left">
-                                                    <a class="news_img" href="#">
-                                                        <img class="media-object" src="{{asset('img/news.jpg')}}" alt="img">
-                                                    </a>
-                                                </div>
-                                                <div class="media-body">
-                                                    <a href="#">Contrary to popular belief, Lorem Ipsum is not simply random text</a>
-                                                    <span class="feed_date">27.02.15</span>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="media">
-                                                <div class="media-left">
-                                                    <a class="news_img" href="#">
-                                                        <img class="media-object" src="{{asset('img/news.jpg')}}" alt="img">
-                                                    </a>
-                                                </div>
-                                                <div class="media-body">
-                                                    <a href="#">Dummy text of the printing and typesetting industry</a>
-                                                    <span class="feed_date">28.02.15</span>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="media">
-                                                <div class="media-left">
-                                                    <a class="news_img" href="#">
-                                                        <img class="media-object" src="{{asset('img/news.jpg')}}" alt="img">
-                                                    </a>
-                                                </div>
-                                                <div class="media-body">
-                                                    <a href="#">Contrary to popular belief, Lorem Ipsum is not simply random text</a>
-                                                    <span class="feed_date">28.02.15</span>
-                                                </div>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
+
                             </div>
                             <!-- Start events tab content -->
                             <div class="tab-pane fade " id="events">
@@ -262,9 +216,7 @@
             </div>
         </div>
     </section>
-    <!--=========== END ABOUT US SECTION ================-->
 
-    <!--=========== END ABOUT US SECTION ===============-->
 
     <!--=========== BEGIN WHY US SECTION ================-->
 
@@ -278,7 +230,7 @@
                         <div class="row">
                             <div class="col-lg-12 col-md-12">
                                 <div class="title_area">
-                                    <h2 class="title_two">Why Us</h2>
+                                    <h2 class="title_two">DMI ENSAH</h2>
                                     <span></span>
                                 </div>
                             </div>
@@ -289,10 +241,10 @@
                             <div class="col-lg-3 col-md-3 col-sm-3">
                                 <div class="single_whyus_top wow fadeInUp">
                                     <div class="whyus_icon">
-                                        <span class="fa fa-desktop"></span>
+                                        <span class="fa fa-android"></span>
                                     </div>
-                                    <h3>Technology</h3>
-                                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                                    <h3>Titre 1</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci alias aut corporis </p>
                                 </div>
                             </div>
                             <div class="col-lg-3 col-md-3 col-sm-3">
@@ -300,25 +252,25 @@
                                     <div class="whyus_icon">
                                         <span class="fa fa-users"></span>
                                     </div>
-                                    <h3>Best Tutos</h3>
-                                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                                    <h3>Titre 2</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque eius hic natus necessitatibus vitae.</p>
                                 </div>
                             </div>
                             <div class="col-lg-3 col-md-3 col-sm-3">
                                 <div class="single_whyus_top wow fadeInUp">
                                     <div class="whyus_icon">
-                                        <span class="fa fa-flask"></span>
+                                        <span class="fa fa-tasks"></span>
                                     </div>
-                                    <h3>Practical Training</h3>
-                                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                                    <h3>Titre 3</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto asperiores quod saepe? aza</p>
                                 </div>
                             </div>
                             <div class="col-lg-3 col-md-3 col-sm-3">
                                 <div class="single_whyus_top wow fadeInUp">
                                     <div class="whyus_icon">
-                                        <span class="fa fa-support"></span>
+                                        <span class="fa fa-calendar"></span>
                                     </div>
-                                    <h3>Support</h3>
+                                    <h3>Titre 4</h3>
                                     <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
                                 </div>
                             </div>
@@ -331,4 +283,14 @@
         <!-- End why us top -->
 
     </section>
+@endsection
+
+@endsection
+@section('script')
+
+    <!-- superslides slider -->
+    <script src="{{asset('js/jquery.easing.1.3.js')}}"></script>
+    <script src="{{asset('js/jquery.animate-enhanced.min.js')}}"></script>
+    <script src="{{asset('js/jquery.superslides.min.js')}}" type="text/javascript" charset="utf-8"></script>
+
 @endsection
